96ece7dcea3d8f1ba5012dcd5820537e2dabeb22
Steve Loveless <steve.loveless@gmail.com>
2013-09-12 01:36:28 -0700

    Added migration to associate user and photo

--- flockr/db/schema.rb
+++ flockr/db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130912063809) do
+ActiveRecord::Schema.define(version: 20130912083203) do
 
   create_table "photos", force: true do |t|
     t.string   "file_name"
     t.string   "path"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "photos", ["user_id"], name: "index_photos_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email"
